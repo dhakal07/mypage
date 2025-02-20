9bbb35913b5bea51da71bca3f17a2eb0dfd90622
Rajiv Dhakal <rdblueblack07@gmail.com>
2025-02-20 14:37:04 +0200

    Initial commit for mypage

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+const App = () => {
+  return (
+    <div>
+      <h1>Welcome to My Page</h1>
+      <p>This is my first React project where I am learning React fundamentals.</p>
+    </div>
+  );
+};
+
+export default App;
